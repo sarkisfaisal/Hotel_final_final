367788a0aa9d457c9a7a2834b65d415edc649d9d
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-18 12:44:33 -0400

    se agrega clase staff

---
+++ staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_final
+{
+    internal class staff
+    {
+        int idturno { get;set}
+        string descripcion { get;set; }
+
+        public staff(int idturno, string descripcion)
+        { 
+            this.idturno = idturno;
+            this.descripcion = descripcion;
+        
+        }
+    }
+}
