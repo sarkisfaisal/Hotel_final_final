78df59fd3e95ba98a1c73fa81a3eb6499b7dafb3
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-18 12:49:01 -0400

    se agrega clase turno

---
+++ turno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_final
+{
+    internal class turno
+    {
+        string tipo { get; set; }
+
+        public turno(string turno)
+        {
+            this.tipo = turno;
+        }
+    }
+}
