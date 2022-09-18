7a19df96c865d96318cb27970f9eb1554276995c
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-18 11:47:50 -0400

    se agrega clase origen

---
+++ origen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Hotel_final
+{
+    internal class origen
+    {
+        string nombre { get; set;}
+
+        public origen(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}
