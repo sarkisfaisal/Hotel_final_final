a0665f2dfb7bb550a22b37a544821b2aca417267
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-18 13:41:13 -0400

    se integran queries sql a program,cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿namespace hotel
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Hotel_final
 {
     class Program
     {
@@ -6,6 +13,17 @@
         static void Main(string[] args)
         {
             
+           
+            conexionbd conexion = new conexionbd();
+            //Console.WriteLine("estado conexion\n");
+            conexion.abrir();
+            Console.WriteLine("ingrese tipo habitacion ");
+            String TipoHabitacion = Console.ReadLine();
+            String insertQuery = "INSERT INTO tipo_habitacion(tipo) VALUES('" + TipoHabitacion + "')";
+            SqlCommand insertCommand = new SqlCommand(insertQuery, conexion.conectarbd);
+            insertCommand.ExecuteNonQuery();
+            conexion.cerrar();
+           
             
             
         
