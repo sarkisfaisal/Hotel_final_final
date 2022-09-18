c08deb8e73fd05987b5328a86c93f714553979ac
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-18 12:38:56 -0400

    se agrega clase tipo_pago

---
+++ tipo_pago.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_final
+{
+    internal class tipo_pago
+    {
+        string descripcion { get; set; }
+
+        public tipo_pago(string descripcion)
+        {
+            this.descripcion = descripcion;
+        }
+    }
+}
