d3e7611452adb60f047e6f233685cdcffca48e2a
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-18 13:02:07 -0400

    se agrega tabla rol

---
+++ rol.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_final
+{
+    internal class rol
+    {
+        private string descripcion { get; set; }
+
+        public rol(string descripcion)
+        {
+            this.descripcion = descripcion;
+        }
+    }
+
+}
