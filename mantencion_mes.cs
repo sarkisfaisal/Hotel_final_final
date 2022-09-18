8c20f0bb05485d1504c41b6ddfd5c4cc7d212069
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-18 11:25:35 -0400

    se agrega mantencion_,mes

---
+++ mantencion_mes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_final
+{
+    internal class mantencion_mes
+    {
+        private string mes { get; set; }
+        private string obs { get; set; }
+
+        public mantencion_mes(string mes, string obs)
+        {
+            this.mes = mes;
+            this.obs = obs;
+        }
+    }
+}
