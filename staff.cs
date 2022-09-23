4420ec1b34016ac1da3e7233e0d7686e7e4cbe78
sarkisfaisal <113647305+sarkisfaisal@users.noreply.github.com>
2022-09-23 13:51:51 -0400

    se crea crud staff

--- staff.cs
+++ staff.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Hotel_final
 {
     internal class staff
     {
-        private int idturno; 
-        string descripcion { get;set; }
+        private int idturno;
+        private string descripcion;
 
         public staff(int idturno, string descripcion)
         { 
@@ -17,5 +19,95 @@ namespace Hotel_final
             this.descripcion = descripcion;
         
         }
+
+        public void SetDescripcion(string descripcion)
+        {
+
+            this.descripcion = descripcion;
+        
+        }
+
+        public string GetDescripcion()
+        {
+            return descripcion;
+        
+        }
+
+
+
+public staff()
+        {
+            //constructor vacío para acceder a las funciones que no requieran nada 
+        }
+
+        public DataTable Listar()
+        {
+            DataTable dtt;
+            conexionbd c = new conexionbd();
+            try
+
+            {
+
+
+                dtt = new DataTable();
+                string selectUsuario = "Select * from staff";
+                SqlCommand cmd = new SqlCommand(selectUsuario, c.conectarbd);
+                cmd.CommandType = CommandType.Text;
+                c.abrir();
+
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                adapter.Fill(dtt);
+                c.cerrar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return dtt;
+        }//fin listar
+
+        public string insertar()
+        {
+            conexionbd c = new conexionbd();
+            try
+            {
+                Console.WriteLine("seleccione turno");
+                Console.WriteLine("1 diurno");
+                Console.WriteLine("2 vespertino");
+                Console.WriteLine("3 nocturno");
+                string idturno = Console.ReadLine();
+                string insert = $"insert into staff values ('{idturno}','{SetDescripcion}')";
+                SqlCommand comando = new SqlCommand(insert, c.conectarbd);
+                c.abrir();
+                comando.ExecuteNonQuery();
+                c.cerrar();
+                return "staff creado con éxito";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }// fin insertar
+
+        public string Eliminar()
+        {
+            conexionbd c = new conexionbd();
+            try
+            {
+                string eliminar = $"delete from staff where staff = '{GetDescripcion}'";
+                SqlCommand comando = new SqlCommand(eliminar, c.conectarbd);
+                c.abrir();
+                comando.ExecuteNonQuery();
+                c.cerrar();
+                return "staff eliminado con éxito";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }//fin eliminar
+
+
     }
 }
